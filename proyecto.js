1f9c50d2c3bc1532998cf944ea68d05b59024026
matiasflores96 <floresmatias18@gmail.com>
2022-03-11 02:32:36 -0300

    actualizacion array

--- proyecto.js
+++ proyecto.js
@@ -1,11 +1,84 @@
-function registrarUsuario() {
-    let nombreUsuario = prompt("Ingrese su nombre");
+
+
+// ARRAYS
+
+// Acá se suben todos los Productos.
+let todosLosProductos = [];
+
+// OBJECTS
+
+class Productos {
+  constructor(producto) {
+    this.producto = producto;
+  }
 }
 
+// FUNCIONES
+
+// Función que registra.
+function registrar() {
+
+  // Se llama a la función para publicar el primer producto y mostrarlo en consola.
+  publicarProducto();
+}
+
+// Función que se utiliza para pedir el producto y transformarlo en un objeto.
+function publicarProducto() {
+  let producto = prompt("Ingrese su producto");
+
+  let productos = new Productos(producto);
+
+  let publicarMasProductos = prompt("¿Deseas comprar otro producto? Responde SOLO POR SI/NO").toLowerCase();
+  tuitASumar = productos.producto; //.producto => Para que devuelva el valor del objeto.
+  todosLosProductos.push(tuitASumar);
+
+  if (publicarMasProductos == "si") {
+    publicarProducto();
+  } else {
+    alert("No mas productos");
+  }
+}
+
+// Función que elimina el último producto.
+function eliminarUltimoProducto(todosLosProductos) {
+  let opcionEliminar = prompt("¿Desea eliminar el último producto?");
+
+  if (opcionEliminar == "si") {
+    todosLosProductos.pop();
+  }
+}
+
+function ordenarArray() {
+  alert("Ahora te voy a mostrar (por consola) el array ordenado de menor a mayor / A-Z");
+
+  // Ordenar el array 'todosLosProductos' de A-Z / 0-100.
+  todosLosProductos.sort((a, b) => {
+    if (a < b) {
+      return -1;
+    }
+
+    if (a > b) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  console.log(todosLosProductos);
+}
 
 // Función que da inicio al registro de usuario.
-registrarUsuario();
+registrar();
 
+// Llamada a la función que elimina el último producto.
+eliminarUltimoProducto(todosLosProductos);
+
+// Llamada a la función que ordena el array de menor a mayor.
+ordenarArray();
+
+
+
+// PAGO TOTAL EN COUTAS.
 
 function convertir(){
     let valore = parseInt(document.getElementById("valor").value);
@@ -16,16 +89,16 @@ function convertir(){
     
     if (document.getElementById("uno").checked){
         resultado = valore * TresCoutas / 100 + valore;
-        alert(`Hola ${nombreUsuario}+su pago es ${resultado} `);
+        alert("su pago es de " + resultado);
     }
     else if (document.getElementById("dos").checked){
         resultado = valore * SeisCoutas / 100 + valore;
-        alert(`Hola ${nombreUsuario}+su pago es ${resultado} `);
+        alert("su pago es de " + resultado);
     }
 
     else if (document.getElementById("tres").checked){
         resultado = valore * DoceCoutas / 100 + valore;
-        alert(`Hola ${nombreUsuario}+su pago es ${resultado} `);
+        alert("su pago es de " + resultado);
     }
     else{
         alert("tienes que completar los datos")
